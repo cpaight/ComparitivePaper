8699e5adbb29ecdec5f8b18b278a9cee7e613dc0
cpaight <cpaight12@gmail.com>
2023-03-23 12:19:23 -0700

    Update MappedtogenomeMRspdes.sh

--- MappedtogenomeMRspdes.sh
+++ MappedtogenomeMRspdes.sh
@@ -1,4 +1,4 @@
 #!
 spades.py --rna -t 16 \
---s1 ~/cpaightNAS/cpaight/data/genome/mrubrumCBJR05/bamfiles/mrub.fq \
--o ~/cpaightNAS/cpaight/data/genome/mrubrumCBJR05/bamfiles/spades
+--s1 ~/data/genome/mrubrumCBJR05/bamfiles/mrub.fq \
+-o ~/data/genome/mrubrumCBJR05/bamfiles/spades
